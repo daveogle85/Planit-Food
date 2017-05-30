dd4cd218680cab67103cf7f70e81fe99867c89c4
CASSIDIMUS <cassiehinkson@msn.com>
2017-05-30 21:00:55 +0100

    Add day card component draft

---
+++ client/app/components/dayCard.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'day-card',
+  templateUrl: '../templates/dayCard.component.html',
+  styleUrls: ['../styles/dayCard.component.scss'],
+})
+export class DayCardComponent { }
--- client/app/modules/app.module.ts
+++ client/app/modules/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from '../components/app.component';
+import { DayCardComponent } from '../components/dayCard.component';
+
 @NgModule({
   imports: [
     BrowserModule,
   ],
   declarations: [
     AppComponent,
+    DayCardComponent
   ],
   bootstrap: [ AppComponent ],
 })
