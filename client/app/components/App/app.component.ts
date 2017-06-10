60de7d4a421a304b43470826f434be9505a57d6f
CASSIDIMUS <cassiehinkson@msn.com>
2017-06-10 21:16:17 +0100

    Initial day card template

--- client/app/components/App/app.component.ts
+++ client/app/components/App/app.component.ts
@@ -10,5 +10,8 @@ export class AppComponent {
     public dateForDayCard = Date.now();
     public mealList: string[] = [
         'Roast Kitty Cat',
+        'Fish and Chips',
+        'Sardines and Rice',
+        'Roast Chicken with Eggs and Slippers and dog foot',
     ];
 }
