14588f0e2ae1e4f0cde6ded4d81c4aa780fc01a1
Dave Ogle <davidogle85@gmail.com>
2020-04-27 10:52:48 +0100

    Add Springboot project for GraphQL server

---
+++ src/main/java/com/planitfood/planitfood/PlanitFoodApplication.java
@@ -0,0 +1,13 @@
+package com.planitfood.planitfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanitFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanitFoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/planitfood/planitfood/PlanitFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.planitfood.planitfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanitFoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
