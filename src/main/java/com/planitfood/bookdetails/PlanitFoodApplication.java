b7098dbf307e6ce63ef1f11acf0cc1be07f793e0
Dave Ogle <davidogle85@gmail.com>
2020-04-27 12:05:54 +0100

    Get basic graphQL example working

--- src/main/java/com/planitfood/bookdetails/PlanitFoodApplication.java
+++ src/main/java/com/planitfood/bookdetails/PlanitFoodApplication.java
@@ -1,4 +1,4 @@
-package com.planitfood.planitfood;
+package com.planitfood.bookdetails;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/planitfood/bookdetails/PlanitFoodApplicationTests.java
+++ src/test/java/com/planitfood/bookdetails/PlanitFoodApplicationTests.java
@@ -1,4 +1,4 @@
-package com.planitfood.planitfood;
+package com.planitfood.bookdetails;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
