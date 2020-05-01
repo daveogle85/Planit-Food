56a981a193e64d2d050047459a9bb0ca1d7236d2
Dave Ogle <davidogle85@gmail.com>
2020-05-01 11:09:24 +0100

    Add REST Api
    Add a Basic REST Api with Tests

---
+++ rest-api/src/test/java/com/planitfood/controllers/IngredientsTests.java
@@ -0,0 +1,29 @@
+package com.planitfood.controllers;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.planitfood.restApi.PlanitFoodApplication;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+
+@ContextConfiguration(classes = PlanitFoodApplication.class)
+@WebMvcTest(IngredientController.class)
+public class IngredientsTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void shouldReturnDefaultMessage() throws Exception {
+        this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("Hello, World")));
+    }
+}
